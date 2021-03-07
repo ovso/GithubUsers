714b16ebfd24ce8217ccf15a6fbf43427b95f144
jaeho <ovsoce@gmail.com>
2021-03-07 20:02:40 +0900

    chore: Remove Spotless, Add .editorconfig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,58 +1,56 @@
 plugins {
 //    id("com.diffplug.gradle.spotless") version "5.11.0"
-    id("com.android.application")
-    id("kotlin-android")
+  id("com.android.application")
+  id("kotlin-android")
 }
 
 android {
-    compileSdkVersion(DefaultConfig.compileSdk)
+  compileSdkVersion(DefaultConfig.compileSdk)
 
-    defaultConfig {
-        applicationId = DefaultConfig.appId
-        minSdkVersion(DefaultConfig.minSdk)
-        targetSdkVersion(DefaultConfig.targetSdk)
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = DefaultConfig.appId
+    minSdkVersion(DefaultConfig.minSdk)
+    targetSdkVersion(DefaultConfig.targetSdk)
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            isDebuggable = false
-            proguardFiles(getDefaultProguardFile(("proguard-android-optimize.txt")))
-            proguardFile(file("proguard-rules.pro"))
-            // library proguard settings
-            val files = rootProject.file("proguard")
-                .listFiles()
-                ?.filter { it.name.startsWith("proguard") }
-                ?.toTypedArray()
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      isDebuggable = false
+      proguardFiles(getDefaultProguardFile(("proguard-android-optimize.txt")))
+      proguardFile(file("proguard-rules.pro"))
+      // library proguard settings
+      val files = rootProject.file("proguard")
+        .listFiles()
+        ?.filter { it.name.startsWith("proguard") }
+        ?.toTypedArray()
 
-            proguardFiles(*files ?: arrayOf())
-        }
-        debug {
-            isDebuggable = true
-        }
+      proguardFiles(*files ?: arrayOf())
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    debug {
+      isDebuggable = true
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+  implementation("androidx.core:core-ktx:1.3.2")
+  implementation("androidx.appcompat:appcompat:1.2.0")
+  implementation("com.google.android.material:material:1.3.0")
+  implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.2")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
-
-//apply(from = "${rootDir}/spotless.gradle")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://plugins.gradle.org/m2/")
-        }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha08")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
-        classpath("com.diffplug.spotless:spotless-plugin-gradle:5.11.0")
-    }
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:7.0.0-alpha08")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+  }
 }
 
-//apply(plugin = "com.diffplug.gradle.spotless")
-
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+  delete(rootProject.buildDir)
 }
